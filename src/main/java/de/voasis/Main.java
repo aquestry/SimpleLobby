b68a940d424e5a71fc24169574d76fcc32fb4684
aquestry <155507142+aquestry@users.noreply.github.com>
2024-10-15 15:20:33 +0200

    Update

--- src/main/java/de/voasis/Main.java
+++ src/main/java/de/voasis/Main.java
@@ -5,6 +5,7 @@ import net.minestom.server.coordinate.Pos;
 import net.minestom.server.entity.Player;
 import net.minestom.server.event.GlobalEventHandler;
 import net.minestom.server.event.player.AsyncPlayerConfigurationEvent;
+import net.minestom.server.event.player.PlayerPacketEvent;
 import net.minestom.server.extras.velocity.VelocityProxy;
 import net.minestom.server.instance.InstanceContainer;
 import net.minestom.server.instance.InstanceManager;
@@ -29,6 +30,10 @@ public class Main {
         }
 
         GlobalEventHandler globalEventHandler = MinecraftServer.getGlobalEventHandler();
+        globalEventHandler.addListener(PlayerPacketEvent.class, event -> {
+            final Player player = event.getPlayer();
+            System.out.println("Player packet event for " + player.getUsername());
+        });
         globalEventHandler.addListener(AsyncPlayerConfigurationEvent.class, event -> {
             final Player player = event.getPlayer();
             System.out.println("Player configuration event for " + player.getUsername());
