edacc81ad38665ed3594d6ca08593a5088504147
aquestry <155507142+aquestry@users.noreply.github.com>
2024-10-27 14:53:18 +0100

    Update

--- src/main/java/de/voasis/Main.java
+++ src/main/java/de/voasis/Main.java
@@ -2,7 +2,6 @@ package de.voasis;
 
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.coordinate.Pos;
-import net.minestom.server.entity.Player;
 import net.minestom.server.event.GlobalEventHandler;
 import net.minestom.server.event.player.AsyncPlayerConfigurationEvent;
 import net.minestom.server.extras.velocity.VelocityProxy;
@@ -17,19 +16,15 @@ public class Main {
         InstanceManager instanceManager = MinecraftServer.getInstanceManager();
         InstanceContainer instanceContainer = instanceManager.createInstanceContainer();
         instanceContainer.setGenerator(unit -> unit.modifier().fillHeight(0, 40, Block.GRASS_BLOCK));
-        String vsecret = System.getenv("PAPER_VELOCITY_SECRET");
-        if(vsecret != null) {
+        if(System.getenv("PAPER_VELOCITY_SECRET") instanceof String vsecret) {
             VelocityProxy.enable(vsecret);
+            System.out.println("v-secret: " + vsecret);
         }
-        System.out.println("v-secret: " + vsecret);
         GlobalEventHandler globalEventHandler = MinecraftServer.getGlobalEventHandler();
         globalEventHandler.addListener(AsyncPlayerConfigurationEvent.class, event -> {
-            final Player player = event.getPlayer();
-            System.out.println("Player configuration event for " + player.getUsername());
             event.setSpawningInstance(instanceContainer);
-            player.setRespawnPoint(new Pos(0, 41, 0));
+            event.getPlayer().setRespawnPoint(new Pos(0, 41, 0));
         });
-
         instanceContainer.setChunkSupplier(LightingChunk::new);
         minecraftServer.start("0.0.0.0", 25565);
     }
