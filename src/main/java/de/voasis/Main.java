50f7df2db986f8b382e7c8590e09d9d74f7ac3b3
aquestry <155507142+aquestry@users.noreply.github.com>
2024-10-27 14:41:09 +0100

    Update

--- src/main/java/de/voasis/Main.java
+++ src/main/java/de/voasis/Main.java
@@ -10,23 +10,18 @@ import net.minestom.server.instance.InstanceContainer;
 import net.minestom.server.instance.InstanceManager;
 import net.minestom.server.instance.LightingChunk;
 import net.minestom.server.instance.block.Block;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("v2 by Aquestry");
         MinecraftServer minecraftServer = MinecraftServer.init();
         InstanceManager instanceManager = MinecraftServer.getInstanceManager();
         InstanceContainer instanceContainer = instanceManager.createInstanceContainer();
         instanceContainer.setGenerator(unit -> unit.modifier().fillHeight(0, 40, Block.GRASS_BLOCK));
-        String vsecret = "SECRET";
-
-        for (String s : args) {
-            if(s.startsWith("SECRET=")) {
-                vsecret = s.replace("SECRET=", "");
-                VelocityProxy.enable(vsecret);
-                System.out.println("v-secret: " + vsecret);
-            }
+        String vsecret = System.getenv("PAPER_VELOCITY_SECRET");
+        if(vsecret != null) {
+            VelocityProxy.enable(vsecret);
         }
-
+        System.out.println("v-secret: " + vsecret);
         GlobalEventHandler globalEventHandler = MinecraftServer.getGlobalEventHandler();
         globalEventHandler.addListener(AsyncPlayerConfigurationEvent.class, event -> {
             final Player player = event.getPlayer();
