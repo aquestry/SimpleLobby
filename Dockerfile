d7b3153f1475b3b1ed5863b9a4ebdf8c120320f7
aquestry <155507142+aquestry@users.noreply.github.com>
2024-11-14 13:40:22 +0100

    Removed Lobby + Cleanup

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,6 @@
 FROM openjdk:21-slim
-
 ENV JAVA_HOME=/usr/local/openjdk-21
 ENV PATH="$JAVA_HOME/bin:$PATH"
-
 WORKDIR /app
-
-COPY target/server.jar server.jar
-COPY target/lobby lobby
-
+COPY target/server.jar /app/server.jar
 CMD ["java", "-jar", "server.jar"]
--- src/main/java/de/voasis/Main.java
+++ src/main/java/de/voasis/Main.java
@@ -8,22 +8,18 @@ import net.minestom.server.event.player.PlayerBlockPlaceEvent;
 import net.minestom.server.event.player.PlayerBlockInteractEvent;
 import net.minestom.server.extras.velocity.VelocityProxy;
 import net.minestom.server.instance.LightingChunk;
-import net.minestom.server.instance.anvil.AnvilLoader;
-import java.nio.file.Path;
+import net.minestom.server.instance.block.Block;
 
 public class Main {
     public static void main(String[] args) {
         var server = MinecraftServer.init();
-        Path lobbyPath = Path.of("lobby");
-        var instance = MinecraftServer.getInstanceManager().createInstanceContainer(new AnvilLoader(lobbyPath));
+        var instance = MinecraftServer.getInstanceManager().createInstanceContainer();
+        instance.setGenerator(unit -> unit.modifier().fillHeight(0, 1, Block.GRASS_BLOCK));
         var vsecret = System.getenv("PAPER_VELOCITY_SECRET");
-        if (vsecret != null) {
-            VelocityProxy.enable(vsecret);
-            System.out.println("v-secret: " + vsecret);
-        }
+        if (vsecret != null) { VelocityProxy.enable(vsecret); }
         MinecraftServer.getGlobalEventHandler().addListener(AsyncPlayerConfigurationEvent.class, event -> {
             event.setSpawningInstance(instance);
-            event.getPlayer().setRespawnPoint(new Pos(383.5, 41, 517.5, 90,0));
+            event.getPlayer().setRespawnPoint(new Pos(0, 41, 517.5, 90,0));
         });
         MinecraftServer.getGlobalEventHandler().addListener(PlayerBlockBreakEvent.class, event -> event.setCancelled(true));
         MinecraftServer.getGlobalEventHandler().addListener(PlayerBlockPlaceEvent.class, event -> event.setCancelled(true));
