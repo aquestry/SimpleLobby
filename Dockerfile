fc52fb11bc41b3b9153dd8491aef018bc3b44f1f
aquestry <155507142+aquestry@users.noreply.github.com>
2024-11-16 13:46:25 +0100

    Fix

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,4 @@ ENV JAVA_HOME=/usr/local/openjdk-21
 ENV PATH="$JAVA_HOME/bin:$PATH"
 WORKDIR /app
 COPY target/server.jar /app/server.jar
-COPY target/world /app/world
 CMD ["java", "-jar", "server.jar"]
--- src/main/java/de/voasis/Main.java
+++ src/main/java/de/voasis/Main.java
@@ -9,16 +9,48 @@ import net.minestom.server.event.player.PlayerBlockInteractEvent;
 import net.minestom.server.extras.velocity.VelocityProxy;
 import net.minestom.server.instance.LightingChunk;
 import net.minestom.server.instance.anvil.AnvilLoader;
+
+import java.io.*;
+import java.net.URL;
+import java.nio.file.*;
+
 public class Main {
     public static void main(String[] args) {
+        File worldDir = new File("world");
+        if (!worldDir.exists()) {
+            worldDir.mkdirs();
+            try {
+                URL regionUrl = Main.class.getResource("/world.region");
+                if (regionUrl != null) {
+                    File regionDir = new File(worldDir, "region");
+                    regionDir.mkdirs();
+                    try (InputStream in = Main.class.getResourceAsStream("/world.region");
+                         BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
+                        String resource;
+                        while ((resource = reader.readLine()) != null) {
+                            if (resource.endsWith(".mca")) {
+                                try (InputStream fileStream = Main.class.getResourceAsStream("/world.region/" + resource)) {
+                                    if (fileStream != null) {
+                                        Files.copy(fileStream, new File(regionDir, resource).toPath(), StandardCopyOption.REPLACE_EXISTING);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                System.err.println("Failed to copy region folder: " + e.getMessage());
+                e.printStackTrace();
+            }
+        }
         var server = MinecraftServer.init();
         var instance = MinecraftServer.getInstanceManager().createInstanceContainer();
-        instance.setChunkLoader(new AnvilLoader("world"));
+        instance.setChunkLoader(new AnvilLoader(worldDir.getPath()));
         var vsecret = System.getenv("PAPER_VELOCITY_SECRET");
         if (vsecret != null) { VelocityProxy.enable(vsecret); }
         MinecraftServer.getGlobalEventHandler().addListener(AsyncPlayerConfigurationEvent.class, event -> {
             event.setSpawningInstance(instance);
-            event.getPlayer().setRespawnPoint(new Pos(0, 100, 0, 0,0));
+            event.getPlayer().setRespawnPoint(new Pos(0, 41, 517.5, 90, 0));
         });
         MinecraftServer.getGlobalEventHandler().addListener(PlayerBlockBreakEvent.class, event -> event.setCancelled(true));
         MinecraftServer.getGlobalEventHandler().addListener(PlayerBlockPlaceEvent.class, event -> event.setCancelled(true));
