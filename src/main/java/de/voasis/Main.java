6972e1b2c8644797e28bf658f8c6adf37e4c3b7f
aquestry <155507142+aquestry@users.noreply.github.com>
2024-12-21 13:50:17 +0100

    Update

--- src/main/java/de/voasis/Main.java
+++ src/main/java/de/voasis/Main.java
@@ -1,5 +1,7 @@
 package de.voasis;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.coordinate.Pos;
 import net.minestom.server.entity.Player;
@@ -12,6 +14,7 @@ import net.minestom.server.network.packet.server.common.PluginMessagePacket;
 import java.nio.charset.StandardCharsets;
 
 public class Main {
+    private static final MiniMessage mm = MiniMessage.miniMessage();
     public static void main(String[] args) {
         Pos npcSpawn = new Pos(0.5, 1, 8.5, 180, 0);
         var server = MinecraftServer.init();
@@ -22,6 +25,7 @@ public class Main {
         instance.setChunkSupplier(LightingChunk::new);
         new NPC(instance, npcSpawn);
         MinecraftServer.getGlobalEventHandler().addListener(AsyncPlayerConfigurationEvent.class, event -> {
+            Player player = event.getPlayer();
             event.setSpawningInstance(instance);
             event.getPlayer().setRespawnPoint(new Pos(0.5, 2, 0.5));
         });
@@ -40,11 +44,19 @@ public class Main {
         MinecraftServer.getGlobalEventHandler().addListener(PlayerPluginMessageEvent.class, event -> {
             String identifier = event.getIdentifier();
             String message = event.getMessageString();
+            Player player = event.getPlayer();
             if(!identifier.equals("nebula:main")) {
                 return;
             }
             System.out.println("Channel: " + identifier + " Message-String:");
             System.out.println(message);
+            instance.sendMessage(Component.text("Channel: " + identifier + " Message-String:"));
+            instance.sendMessage(Component.text(message));
+            player.setDisplayName(mm.deserialize(message.split(":")[1].split("#")[2]));
+            NametagEntity tag = new NametagEntity(player);
+            for(Player p : instance.getPlayers()) {
+                tag.updateNewViewer(p);
+            }
         });
         MinecraftServer.getGlobalEventHandler().addListener(PlayerBlockBreakEvent.class, event -> event.setCancelled(true));
         MinecraftServer.getGlobalEventHandler().addListener(PlayerBlockPlaceEvent.class, event -> event.setCancelled(true));
