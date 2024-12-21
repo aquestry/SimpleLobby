20937ec00055fc84ba714fffebee567eecf14fc6
aquestry <155507142+aquestry@users.noreply.github.com>
2024-12-21 17:45:18 +0100

    Update

--- src/main/java/de/voasis/NPC.java
+++ src/main/java/de/voasis/NPC.java
@@ -1,7 +1,11 @@
 package de.voasis;
 
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.minestom.server.coordinate.Pos;
+import net.minestom.server.coordinate.Vec;
 import net.minestom.server.entity.*;
+import net.minestom.server.entity.metadata.display.AbstractDisplayMeta;
+import net.minestom.server.entity.metadata.display.TextDisplayMeta;
 import net.minestom.server.instance.Instance;
 import net.minestom.server.network.packet.server.play.EntityMetaDataPacket;
 import net.minestom.server.network.packet.server.play.PlayerInfoUpdatePacket;
@@ -16,6 +20,14 @@ public class NPC extends Entity {
         setNoGravity(true);
         setInstance(instance);
         scheduleNextTick(entity -> teleport(new Pos(0.5, 1, 8.5, 180, 0)));
+        Entity entity = new Entity(EntityType.TEXT_DISPLAY);
+        TextDisplayMeta meta = (TextDisplayMeta) entity.getEntityMeta();
+        meta.setText(MiniMessage.miniMessage().deserialize("<bold>Parkour"));
+        meta.setBillboardRenderConstraints(AbstractDisplayMeta.BillboardConstraints.CENTER);
+        meta.setBackgroundColor(0x00000000);
+        meta.setShadow(true);
+        meta.setTranslation(new Vec(0, 0.3, 0));
+        this.addPassenger(entity);
     }
     @Override
     public void updateNewViewer(@NotNull Player player) {
--- src/main/java/de/voasis/NameTagHandler.java
+++ src/main/java/de/voasis/NameTagHandler.java
@@ -2,10 +2,12 @@ package de.voasis;
 
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.minestom.server.MinecraftServer;
+import net.minestom.server.coordinate.Vec;
 import net.minestom.server.entity.Entity;
 import net.minestom.server.entity.EntityType;
 import net.minestom.server.entity.Player;
-import net.minestom.server.entity.metadata.other.InteractionMeta;
+import net.minestom.server.entity.metadata.display.AbstractDisplayMeta;
+import net.minestom.server.entity.metadata.display.TextDisplayMeta;
 import net.minestom.server.event.player.PlayerDisconnectEvent;
 import net.minestom.server.event.player.PlayerPluginMessageEvent;
 
@@ -18,16 +20,18 @@ public class NameTagHandler {
             if(!identifier.equals("nebula:main")) { return; }
             System.out.println("Channel: " + identifier + " Message:");
             System.out.println(message);
-            Entity display = new Entity(EntityType.INTERACTION);
             String playerName = message.split(":")[0];
             Player player = MinecraftServer.getConnectionManager().getOnlinePlayerByUsername(playerName);
             if(player != null && player.getPassengers().isEmpty()) {
                 player.setDisplayName(MiniMessage.miniMessage().deserialize(message.split(":")[1].split("#")[2] + player.getUsername()));
-                InteractionMeta displayMeta = (InteractionMeta) display.getEntityMeta();
-                displayMeta.setInvisible(true);
-                displayMeta.setCustomName(player.getDisplayName());
-                displayMeta.setCustomNameVisible(true);
-                player.addPassenger(display);
+                Entity entity = new Entity(EntityType.TEXT_DISPLAY);
+                TextDisplayMeta meta = (TextDisplayMeta) entity.getEntityMeta();
+                meta.setText(player.getDisplayName());
+                meta.setBillboardRenderConstraints(AbstractDisplayMeta.BillboardConstraints.CENTER);
+                meta.setBackgroundColor(0x00000000);
+                meta.setShadow(true);
+                meta.setTranslation(new Vec(0, 0.3, 0));
+                player.addPassenger(entity);
             }
         });
     }
