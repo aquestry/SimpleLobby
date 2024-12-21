09c620777ceaee48187d3d8d167aa54c0647173b
aquestry <155507142+aquestry@users.noreply.github.com>
2024-12-21 17:07:35 +0100

    Update

--- src/main/java/de/voasis/Main.java
+++ src/main/java/de/voasis/Main.java
@@ -1,12 +1,8 @@
 package de.voasis;
 
-import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.coordinate.Pos;
-import net.minestom.server.entity.Entity;
-import net.minestom.server.entity.EntityType;
 import net.minestom.server.entity.Player;
-import net.minestom.server.entity.metadata.other.InteractionMeta;
 import net.minestom.server.event.entity.EntityAttackEvent;
 import net.minestom.server.event.player.*;
 import net.minestom.server.extras.velocity.VelocityProxy;
@@ -16,7 +12,6 @@ import net.minestom.server.network.packet.server.common.PluginMessagePacket;
 import java.nio.charset.StandardCharsets;
 
 public class Main {
-    private static final MiniMessage mm = MiniMessage.miniMessage();
     public static void main(String[] args) {
         Pos npcSpawn = new Pos(0.5, 1, 8.5, 180, 0);
         var server = MinecraftServer.init();
@@ -31,7 +26,6 @@ public class Main {
             event.setSpawningInstance(instance);
             event.getPlayer().setRespawnPoint(new Pos(0.5, 2, 0.5));
         });
-        MinecraftServer.getGlobalEventHandler().addListener(PlayerDisconnectEvent.class, event -> event.getPlayer().getPassengers().forEach(Entity::remove));
         MinecraftServer.getGlobalEventHandler().addListener(EntityAttackEvent.class, event -> {
             if(event.getEntity() instanceof Player player) {
                 if(event.getTarget().getPosition().equals(npcSpawn)) {
@@ -42,26 +36,6 @@ public class Main {
                     );
                     player.sendPacket(packet);
                 }
-            }
-        });
-        MinecraftServer.getGlobalEventHandler().addListener(PlayerPluginMessageEvent.class, event -> {
-            String identifier = event.getIdentifier();
-            String message = event.getMessageString();
-            if(!identifier.equals("nebula:main")) {
-                return;
-            }
-            System.out.println("Channel: " + identifier + " Message:");
-            System.out.println(message);
-            Entity display = new Entity(EntityType.INTERACTION);
-            String playerName = message.split(":")[0];
-            Player player = MinecraftServer.getConnectionManager().getOnlinePlayerByUsername(playerName);
-            if(player != null && player.getPassengers().isEmpty()) {
-                player.setDisplayName(mm.deserialize(message.split(":")[1].split("#")[2] + player.getUsername()));
-                InteractionMeta displayMeta = (InteractionMeta) display.getEntityMeta();
-                displayMeta.setInvisible(true);
-                displayMeta.setCustomName(player.getDisplayName());
-                displayMeta.setCustomNameVisible(true);
-                player.addPassenger(display);
             }
         });
         MinecraftServer.getGlobalEventHandler().addListener(PlayerBlockBreakEvent.class, event -> event.setCancelled(true));
---
+++ src/main/java/de/voasis/NameTagHandler.java
@@ -0,0 +1,35 @@
+package de.voasis;
+
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.minestom.server.MinecraftServer;
+import net.minestom.server.entity.Entity;
+import net.minestom.server.entity.EntityType;
+import net.minestom.server.entity.Player;
+import net.minestom.server.entity.metadata.other.InteractionMeta;
+import net.minestom.server.event.player.PlayerDisconnectEvent;
+import net.minestom.server.event.player.PlayerPluginMessageEvent;
+
+public class NameTagHandler {
+    private static final MiniMessage mm = MiniMessage.miniMessage();
+    public NameTagHandler() {
+        MinecraftServer.getGlobalEventHandler().addListener(PlayerDisconnectEvent.class, event -> event.getPlayer().getPassengers().forEach(Entity::remove));
+        MinecraftServer.getGlobalEventHandler().addListener(PlayerPluginMessageEvent.class, event -> {
+            String identifier = event.getIdentifier();
+            String message = event.getMessageString();
+            if(!identifier.equals("nebula:main")) { return; }
+            System.out.println("Channel: " + identifier + " Message:");
+            System.out.println(message);
+            Entity display = new Entity(EntityType.INTERACTION);
+            String playerName = message.split(":")[0];
+            Player player = MinecraftServer.getConnectionManager().getOnlinePlayerByUsername(playerName);
+            if(player != null && player.getPassengers().isEmpty()) {
+                player.setDisplayName(mm.deserialize(message.split(":")[1].split("#")[2] + player.getUsername()));
+                InteractionMeta displayMeta = (InteractionMeta) display.getEntityMeta();
+                displayMeta.setInvisible(true);
+                displayMeta.setCustomName(player.getDisplayName());
+                displayMeta.setCustomNameVisible(true);
+                player.addPassenger(display);
+            }
+        });
+    }
+}
